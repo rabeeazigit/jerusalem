9f813b6198b2066405eabcf59909d2724b7cf7e8
rabeea <rabeea.s@zigit.co.il>
2025-01-28 10:45:26 +0200

    - fixed event date and time display format.

--- template-parts/event-card.php
+++ template-parts/event-card.php
@@ -1,12 +1,20 @@
 <?php
 $event_name = $args["event_name"] ?? null;
-$event_occurrence_date = $args["event_occurrence_date"] ?? null;
+$event_occurrence_date = $args["event_occurrence_date"] ?? "";
 $event_place = $args["event_place"] ?? null;
 $event_card_image = $args["event_card_image"] ?? null;
 $event_card_short_description = $args["event_card_short_description"] ?? null;
 $event_card_short_button_text = $args["event_card_short_button_text"] ?? null;
 $permalink = $args["permalink"] ?? "#";
 $post_type = $args["post_type"] ?? null;
+
+if (!empty($event_occurrence_date) && strpos($event_occurrence_date, ' ') !== false) {
+    $event_date = explode(" ", $event_occurrence_date)[0];
+    $event_time = explode(" ", $event_occurrence_date)[1];
+} else {
+    $event_date = null;
+    $event_time = null;
+}
 
 switch ($post_type) {
     case "event":
@@ -40,9 +48,17 @@ switch ($post_type) {
         <?php endif; ?>
 
         <div class="hstack px-2 align-items-center gap-3 opacity-75 mb-3">
-            <?php if ($event_occurrence_date) : ?>
+            <?php if ($event_date) : ?>
                 <div>
-                    <?= $event_occurrence_date; ?>
+                    <?= $event_date; ?>
+                </div>
+
+                <div class="vr"></div>
+            <?php endif; ?>
+
+            <?php if ($event_time) : ?>
+                <div>
+                    <?= $event_time; ?>
                 </div>
 
                 <div class="vr"></div>
