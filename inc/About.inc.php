729f29304e2936775bd7ceec13bab012b8b1f21a
rabeea <rabeea.s@zigit.co.il>
2025-02-23 13:48:23 +0200

    staff email working

--- inc/About.inc.php
+++ inc/About.inc.php
@@ -12,13 +12,13 @@ class About
     private $hero_image;
     private $bk_sec_about;
     private $bk_sec_activities;
-    private $pid ;
+    private $pid;
 
 
 
     public function __construct()
     {
-        $this->pid = get_the_ID() ;
+        $this->pid = get_the_ID();
         $this->about_content = get_field('about_content', $this->pid);
         $this->tagd_links = get_field('tagd_links', $this->pid);
         $this->about_sexy_numbers = get_field('about_sexy_numbers', $this->pid);
@@ -29,26 +29,24 @@ class About
         $this->hero_image = get_field('hero_image', $this->pid);
         $this->bk_sec_about = get_field('bk_sec_about', $this->pid);
         $this->bk_sec_activities = get_field('bk_sec_activities', $this->pid);
-
-
     }
 
 
 
 
-private function TagTheFucker($tags = []){
-$html = '';
+    private function TagTheFucker($tags = [])
+    {
+        $html = '';
 
-foreach($tags as $key=>$tag){
-//$tag[$key]['text'] =$val['tagd_links_text'];
-//$tag[$key]['link'] =$val['tagd_links_url'];
-$html .= "<div class='btn btn-outline-secondary rounded-pill hovertagabout m-2'>
+        foreach ($tags as $key => $tag) {
+            //$tag[$key]['text'] =$val['tagd_links_text'];
+            //$tag[$key]['link'] =$val['tagd_links_url'];
+            $html .= "<div class='btn btn-outline-secondary rounded-pill hovertagabout m-2'>
 <a href='{$tag['tagd_links_url']}' target='_BLANK'>{$tag['tagd_links_text']}</a></div>";
-}
+        }
 
-return $html;
-
-}
+        return $html;
+    }
 
 
 
@@ -59,7 +57,7 @@ return $html;
         $html = '<div class="container-fluid px-0">
             <div class="row">
                 <div class="col">
-                '. get_template_part("template-parts/navbar") . ' 
+                ' . get_template_part("template-parts/navbar") . ' 
                 </div> 
         </div>';
 
@@ -72,18 +70,18 @@ return $html;
     {
 
         $bk = $this->bk_sec_about ? $this->bk_sec_about : '';
-        $tags = $this->TagTheFucker( $this->tagd_links  );
+        $tags = $this->TagTheFucker($this->tagd_links);
         $html = '
 
-    <div class="container-fluid mt-5 secssionBk" style="background:url('.$bk.');">
+    <div class="container-fluid mt-5 secssionBk" style="background:url(' . $bk . ');">
             <div class="hero-section p-4">
                 <div class="row align-items-start">
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 about_text ">
-                        <h1 class="aboutTitle display-4 fw-bold">'.get_the_title(get_the_ID()).'</h1>
-                        <span class="fs-5">'.$this->about_content.'</span><br>';
-                        $html .=$tags . '</div>
+                        <h1 class="aboutTitle display-4 fw-bold">' . get_the_title(get_the_ID()) . '</h1>
+                        <span class="fs-5">' . $this->about_content . '</span><br>';
+        $html .= $tags . '</div>
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                        <img src="'.$this->hero_image.'" alt="" class="img-fluid rounded">
+                        <img src="' . $this->hero_image . '" alt="" class="img-fluid rounded">
                     </div>
                 </div>
             </div>
@@ -102,9 +100,9 @@ return $html;
        <div class="row py-5 my-2">';
         foreach ($SexyNumbers as $numbers) {
             $html .=
-            '<div class="col text-center text-dark">
-              <div class="display-1 fw-semibold">'. $numbers['about_number'] .'</div>
-              <div class="fs-5 fw-semibold">'. $numbers['about_numbers_text'] .'</div>
+                '<div class="col text-center text-dark">
+              <div class="display-1 fw-semibold">' . $numbers['about_number'] . '</div>
+              <div class="fs-5 fw-semibold">' . $numbers['about_numbers_text'] . '</div>
               </div>';
         }
 
@@ -127,35 +125,38 @@ return $html;
 
     public function OurStaff()
     {
-        $Staff =  $this->our_staff ;
+        $Staff =  $this->our_staff;
 
         $html = '
  <div class="container">
        <div class="d-flex flex-wrap align-items-center justify-content-around py-5 my-2">
 ';
         foreach ($Staff as $Staff) {
-            $html .= 
-            '<div class="col-lg-3 mb-5">
-                        <div class="staff_photo"><img src="'.$Staff['staff_member_photo'].'" loading="lazy"/>
-                        </div>
-                        <div class="container cont_det">
-                        <div class="row">
-                        <div class="col-12 fs-5 fw-bold">'.$Staff['staff_member_name'].'</div>
-                       
-                        <div class="col-12">'.$Staff['staff_member_role'].'</div>
-                        <div class="col-12"><span><span><img src="'. get_template_directory_uri().'/assets/images/about/email-icon.png" /></span>'.$Staff['staff_member_email'].'</span></div>
-                        </div>
-                        </div>
+            $html .=
+                '<div class="col-xl-3 col-md-4 col-6 mb-5">
+                    <div class="staff_photo">
+                        <img class="img-fluid object-fit-cover" src="' . $Staff['staff_member_photo'] . '" loading="lazy"/>
+                    </div>
 
+                    <div class="container cont_det">
+                        <div class="row">
+                            <div class="col-12 fs-5 fw-bold">' . $Staff['staff_member_name'] . '</div>
+                            
+                            <div class="col-12">' . $Staff['staff_member_role'] . '</div>
+
+                            <div class="col-12">
+                                <a href="mailto: ' . $Staff['staff_member_email'] . '" class="hstack gap-2 align-items-center text-reset text-decoration-none">
+                                    <img src="' . get_template_directory_uri() . '/assets/images/about/email-icon.png" />
+                                    <div class="fs-6">' . $Staff['staff_member_email'] . '</div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
             </div>';
         }
 
         $html .= '</div></div>';
 
         return $html;
-
     }
-
-
-
 }//END OF CLASS
