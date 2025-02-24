1207bc1d0e1d08e31ad5d84595308fe88eb51a62
rabeea <rabeea.s@zigit.co.il>
2025-02-24 11:39:37 +0200

    - added new global.js that is loaded onto every request, careful with content. - added script to show/hide top navbar with scroll.

---
+++ assets/js/global.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Get the top navbar element
+    const topNavbar = document.querySelector("#top-navbar");
+
+    // Listen for user scrolling
+    document.addEventListener("scroll", (event) => {
+        // Get the current scrollY of the screen
+        const currentY = window.scrollY;
+
+        // If the user is at 100px on the y-axis
+        // The top navbar should be hidden
+        // Else it's shown regularly
+        if (currentY >= 100) {
+            topNavbar.style.top = "-80px";
+        } else {
+            topNavbar.style.top = "0";
+        }
+    });
+});
