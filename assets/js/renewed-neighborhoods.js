ed29de691dc9b4bdbc0ab41e758c34fdba37a308
rabeea <rabeea.s@zigit.co.il>
2025-04-16 10:06:21 +0300

    formatting fix

--- assets/js/renewed-neighborhoods.js
+++ assets/js/renewed-neighborhoods.js
@@ -2,44 +2,44 @@ $(() => {
     // handles the filtering logic
     $("#neighborhoods_search")
         .on("input", function (event) {
-        const formData = new FormData(event.currentTarget);
+            const formData = new FormData(event.currentTarget);
 
-        const data = {
-            neighborhood: formData.get("neighborhood")?.toString() ?? null,
-            projectStatus: formData.get("project_status")?.toString() ?? null,
-            query: formData.get("query")?.toString() ?? null,
-            action: "get_filtered_projects",
-            nonce: ajaxObject.nonce,
-        };
+            const data = {
+                neighborhood: formData.get("neighborhood")?.toString() ?? null,
+                projectStatus: formData.get("project_status")?.toString() ?? null,
+                query: formData.get("query")?.toString() ?? null,
+                action: "get_filtered_projects",
+                nonce: ajaxObject.nonce,
+            };
 
-        // if the filter is cleared
-        if (!data.neighborhood && !data.projectStatus && !data.query) {
-            window.location.reload();
-            return;
-        }
+            // if the filter is cleared
+            if (!data.neighborhood && !data.projectStatus && !data.query) {
+                window.location.reload();
+                return;
+            }
 
-        $.ajax({
-            url: ajaxObject.ajaxUrl,
-            method: "POST",
-            data,
-            dataType: "json",
-            beforeSend: function () {
-                $(this).prop("disabled", true);
-            },
-            success: function (response) {
-                const projects = response.projects ?? null;
+            $.ajax({
+                url: ajaxObject.ajaxUrl,
+                method: "POST",
+                data,
+                dataType: "json",
+                beforeSend: function () {
+                    $(this).prop("disabled", true);
+                },
+                success: function (response) {
+                    const projects = response.projects ?? null;
 
-                $("#loadMoreProjects").fadeOut(function () {
-                    $("#projects-container").html(projects);
-                });
-            },
-            error: function (error) {
-                console.error(error);
-            },
-            complete: function () {
-                $(this).prop("disabled", false);
-            },
-        });
+                    $("#loadMoreProjects").fadeOut(function () {
+                        $("#projects-container").html(projects);
+                    });
+                },
+                error: function (error) {
+                    console.error(error);
+                },
+                complete: function () {
+                    $(this).prop("disabled", false);
+                },
+            });
         })
         .on("submit", function (event) {
             event.preventDefault();
