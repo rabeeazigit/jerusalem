8f9fd441be8c00a7543340038e1282a84c1abdab
rabeea <rabeea.s@zigit.co.il>
2025-04-23 14:36:36 +0300

    updated title attribute

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        <?= bloginfo("title"); ?>
+        <?= wp_title("title"); ?>
     </title>
     <?php wp_head(); ?>
 </head>
